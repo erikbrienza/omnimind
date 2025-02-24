ee78b7138b87792b469d91d588114c5d521c2cdd
Erik Brienza <erikbrienza@IPHONE.station>
2025-02-24 13:34:58 +0100

    Rimosse librerie di testing e script test

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/meditation" element={<Meditation />} />
-        <Route path="/info" element={<Info />} />
       </Routes>
     </div>
   );
